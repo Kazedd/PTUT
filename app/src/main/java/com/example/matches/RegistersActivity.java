c675925a5e692691f1c451becc166e7707f8bf5c
brendan <brendanfrs1@outlook.com>
2020-10-26 19:41:22 +0100

    commit de l'ajout pour la photo et modifs dans le layout pour bien tout aligner

--- app/src/main/java/com/example/matches/RegistersActivity.java
+++ app/src/main/java/com/example/matches/RegistersActivity.java
@@ -1,11 +1,18 @@
 package com.example.matches;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -25,10 +32,12 @@ import java.util.Map;
 
 public class RegistersActivity extends AppCompatActivity {
     EditText nom, prenom, adresseMail, motDePasse, telephone, adresse, age;
-    Button register;
+    Button register, photo;
     FirebaseAuth firebaseAuth;
     FirebaseFirestore firestore;
     String userID;
+    final static int SELECT_PICTURE = 1;
+    ImageView imagePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +52,13 @@ public class RegistersActivity extends AppCompatActivity {
         adresse = (EditText) findViewById(R.id.adresse);
         age = (EditText) findViewById(R.id.age);
         register = (Button) findViewById(R.id.register);
+        imagePhoto = (ImageView) findViewById(R.id.imagePhoto);
+        ((Button) findViewById(R.id.photo)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btGalleryClick(v);
+            }
+        });
 
         firebaseAuth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
@@ -130,5 +146,42 @@ public class RegistersActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+    public void btGalleryClick(View v) {
+        //Création puis ouverture de la boite de dialogue
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, ""), SELECT_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case SELECT_PICTURE:
+                    String path = getRealPathFromURI(data.getData());
+                    Log.d("Choose Picture", path);
+                    //Transformer la photo en Bitmap
+                    Bitmap bitmap = BitmapFactory.decodeFile(path);
+                    //Afficher le Bitmap
+                    imagePhoto.setImageBitmap(bitmap);
+                    break;
+            }
+        }
+    }
+    private String getRealPathFromURI(Uri contentURI) {
+        String result;
+        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) {
+            result = contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
+        return result;
     }
 }
